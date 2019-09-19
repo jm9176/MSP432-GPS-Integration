0485498415e935e6b6fe98d7718b33824a90c8c1
Shubhdildeep Singh Sohal <shubh94@vt.edu>
2019-09-18 22:56:28 -0400

    Add files via upload

---
+++ GPS-MSP432-main.c
@@ -0,0 +1,236 @@
+/* --COPYRIGHT--,BSD
+ * Copyright (c) 2017, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * --/COPYRIGHT--*/
+/******************************************************************************
+ * MSP432 UART - PC Echo with 12MHz BRCLK
+ *
+ * Description: This demo echoes back characters received via a PC serial port.
+ * SMCLK/DCO is used as a clock source and the device is put in LPM0
+ * The auto-clock enable feature is used by the eUSCI and SMCLK is turned off
+ * when the UART is idle and turned on when a receive edge is detected.
+ * Note that level shifter hardware is needed to shift between RS232 and MSP
+ * voltage levels.
+ *
+ *               MSP432P401
+ *             -----------------
+ *            |                 |
+ *            |                 |
+ *            |                 |
+ *       RST -|     P1.3/UCA0TXD|----> PC (echo)
+ *            |                 |
+ *            |                 |
+ *            |     P1.2/UCA0RXD|<---- PC
+ *            |                 |
+ *
+ *******************************************************************************/
+
+/*
+   $GPBOD - Bearing, origin to destination
+   $GPBWC - Bearing and distance to waypoint, great circle
+   $GPGGA - Global Positioning System Fix Data
+   $GPGLL - Geographic position, latitude / longitude
+   $GPGSA - GPS DOP and active satellites
+   $GPGSV - GPS Satellites in view
+   $GPHDT - Heading, True
+   $GPR00 - List of waypoints in currently active route
+   $GPRMA - Recommended minimum specific Loran-C data
+   $GPRMB - Recommended minimum navigation info
+   $GPRMC - Recommended minimum specific GPS/Transit data
+   $GPRTE - Routes
+   $GPTRF - Transit Fix Data
+   $GPSTN - Multiple Data ID
+   $GPVBW - Dual Ground / Water Speed
+   $GPVTG - Track made good and ground speed
+   $GPWPL - Waypoint location
+   $GPXTE - Cross-track error, Measured
+   $GPZDA - Date & Time
+
+   link: http://aprs.gids.nl/nmea/
+
+   eg2. $--GGA,hhmmss.ss,llll.ll,a,yyyyy.yy,a,x,xx,x.x,x.x,M,x.x,M,x.x,xxxx
+
+hhmmss.ss = UTC of position
+llll.ll = latitude of position
+a = N or S
+yyyyy.yy = Longitude of position
+a = E or W
+x = GPS Quality indicator (0=no fix, 1=GPS fix, 2=Dif. GPS fix)
+xx = number of satellites in use
+x.x = horizontal dilution of precision
+x.x = Antenna altitude above mean-sea-level
+M = units of antenna altitude, meters
+x.x = Geoidal separation
+M = units of geoidal separation, meters
+x.x = Age of Differential GPS data (seconds)
+xxxx = Differential reference station ID
+*/
+
+
+/* DriverLib Includes */
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+
+/* Standard Includes */
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "printf.h"
+
+// defining the buffer size for the char array to
+// store each byte of received sensor data
+#define buffer_size 75
+
+char rxBuffer[buffer_size];
+char RXData;
+
+// index initializer for the rxBuffer
+uint8_t rxWriteIndex = 0;
+
+// data write enabler for the exBuffer
+uint8_t writeData = 0;
+
+//![Simple UART Config]
+/* UART Configuration Parameter. These are the configuration parameters to
+ * make the eUSCI A UART module to operate with a 9600 baud rate. These
+ * values were calculated using the online calculator that TI provides
+ * at:
+ *http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSP430BaudRateConverter/index.html
+ */
+const eUSCI_UART_Config uartConfig =
+{
+        EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
+        78,                                     // BRDIV = 78
+        2,                                       // UCxBRF = 2
+        0,                                       // UCxBRS = 0
+        EUSCI_A_UART_NO_PARITY,                  // No Parity
+        EUSCI_A_UART_LSB_FIRST,                  // LSB First
+        EUSCI_A_UART_ONE_STOP_BIT,               // One stop bit
+        EUSCI_A_UART_MODE,                       // UART mode
+        EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION  // Oversampling
+};
+//![Simple UART Config]
+
+int main(void)
+{
+    /* Halting WDT  */
+    MAP_WDT_A_holdTimer();
+
+    /* Selecting P1.2 and P1.3 in UART mode */
+    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,
+            GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+
+    /* Selecting P3.2 and P3.3 in UART mode */
+    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P3,
+            GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+
+    /* Setting DCO to 12MHz */
+    CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_12);
+
+    /* Configuring UART Module 0*/
+    MAP_UART_initModule(EUSCI_A0_BASE, &uartConfig);
+
+    /* Enable UART module */
+    MAP_UART_enableModule(EUSCI_A0_BASE);
+
+    /* Enabling interrupts */
+    MAP_UART_enableInterrupt(EUSCI_A0_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT);
+    MAP_Interrupt_enableInterrupt(INT_EUSCIA0);
+    MAP_Interrupt_enableSleepOnIsrExit();
+
+    /* Configuring UART Module 2*/
+    MAP_UART_initModule(EUSCI_A2_BASE, &uartConfig);
+
+    /* Enable UART module */
+    MAP_UART_enableModule(EUSCI_A2_BASE);
+
+    /* Enabling interrupts */
+    MAP_UART_enableInterrupt(EUSCI_A2_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT);
+    MAP_Interrupt_enableInterrupt(INT_EUSCIA2);
+    MAP_Interrupt_enableSleepOnIsrExit();
+
+    while(1)
+    {
+        MAP_PCM_gotoLPM0InterruptSafe();
+    }
+}
+
+/*Printing data received at the PC host to the terminal*/
+void EUSCIA0_IRQHandler(void)
+{
+    uint32_t status = MAP_UART_getEnabledInterruptStatus(EUSCI_A0_BASE);
+    MAP_UART_clearInterruptFlag(EUSCI_A0_BASE, status);
+
+    if(status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG)
+    {
+        // Printing the sensor data to the terminal (COM PORT 4)
+        // refer to the printf.c file in the code directory for the following function
+        printf(EUSCI_A0_BASE, "%s\r\n", MAP_UART_receiveData(EUSCI_A0_BASE));
+    }
+
+    MAP_Interrupt_disableSleepOnIsrExit();
+}
+
+/* EUSCI A0 UART ISR - Echoes data from P3 (EUSCI_A2_BASE) to PC (EUSCI_A0_BASE) host */
+void EUSCIA2_IRQHandler(void)
+{
+    uint32_t status = MAP_UART_getEnabledInterruptStatus(EUSCI_A2_BASE);
+
+    MAP_UART_clearInterruptFlag(EUSCI_A2_BASE, status);
+
+    if(status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG)
+    {
+        // Receive the data using the sensor connected to PORT3 UART
+        RXData = MAP_UART_receiveData(EUSCI_A2_BASE);
+
+        // Transmit the data received on PORT3 to be printed using the PORT1
+        MAP_UART_transmitData(EUSCI_A0_BASE, MAP_UART_receiveData(EUSCI_A2_BASE));
+
+        // Check for the start of sensor data, initializing the rxBuffer index
+        // and enable the writing of data
+        if(RXData == '$'){
+            rxWriteIndex = 0;
+            writeData = 1;
+        }
+
+        // writing the sensor data to the rxBuffer of size 75 bytes
+        if(writeData){
+            rxBuffer[rxWriteIndex++] = RXData;
+        }
+
+        // check for the end of data
+        if(RXData == '\r'){
+            rxBuffer[rxWriteIndex] = '\0';
+            writeData = 0;
+        }
+    }
+
+    MAP_Interrupt_disableSleepOnIsrExit();
+}
+
